ce113868c6cb4d8adf4a8736f2bc258e49d0bc3b
uponup <1030360567@qq.com>
2024-06-23 14:55:46 +0800

    add cookbook

---
+++ cookbook/Demo/Tools/airdrop_sol.ts
@@ -0,0 +1,16 @@
+import { Connection, Keypair, LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+(async () => {
+    const keypair = Keypair.generate();
+    const connection = new Connection("http://127.0.0.1:8899", "confirmed")
+    const signature = await connection.requestAirdrop(
+        keypair.publicKey,
+        LAMPORTS_PER_SOL
+    );
+    const { blockhash, lastValidBlockHeight } = await connection.getLatestBlockhash()
+    await connection.confirmTransaction({
+        blockhash,
+        lastValidBlockHeight,
+        signature
+    });
+})();
